98355d5db9d4b5bf81a60589f60acf3d069372ad
Arroyo75 <jszczepanski75@gmail.com>
2026-03-06 16:41:14 +0100

    First commit - empty user service

---
+++ user-service/src/main/java/com/baw/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.baw.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
