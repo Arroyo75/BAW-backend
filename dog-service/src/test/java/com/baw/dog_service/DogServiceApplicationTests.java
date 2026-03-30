e285f6b6b0042f235748a7b9aae1ecbbe2512331
Arroyo75 <jszczepanski75@gmail.com>
2026-03-30 20:14:33 +0200

    Two remaining pipelines

--- dog-service/src/test/java/com/baw/dog_service/DogServiceApplicationTests.java
+++ dog-service/src/test/java/com/baw/dog_service/DogServiceApplicationTests.java
@@ -3,7 +3,6 @@ package com.baw.dog_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DogServiceApplicationTests {
 
 	@Test
--- rating-service/src/test/java/com/baw/rating_service/RatingServiceApplicationTests.java
+++ rating-service/src/test/java/com/baw/rating_service/RatingServiceApplicationTests.java
@@ -3,7 +3,6 @@ package com.baw.rating_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class RatingServiceApplicationTests {
 
 	@Test
