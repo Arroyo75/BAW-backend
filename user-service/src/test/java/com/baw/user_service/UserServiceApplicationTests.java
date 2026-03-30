2c39ff12549b5ff06daedc9211109f157e534a0b
Arroyo75 <jszczepanski75@gmail.com>
2026-03-30 18:39:35 +0200

    Pipeline fix #3

--- user-service/src/test/java/com/baw/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/baw/user_service/UserServiceApplicationTests.java
@@ -1,11 +1,20 @@
 package com.baw.user_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 class UserServiceApplicationTests {
 
 	@Test
